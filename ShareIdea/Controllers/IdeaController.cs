96ab56cc91285f8074de02959914f56b11af3948
Andrei_Egorov1 <andrei_egorov1@epam.com>
2016-06-17 18:35:16 +0300

    first commit

---
+++ ShareIdea/Controllers/IdeaController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareIdea.Controllers
+{
+    public class IdeaController : Controller
+    {
+        //
+        // GET: /Idea/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Idea/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Idea/Create
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        //
+        // POST: /Idea/Create
+
+        [HttpPost]
+        public ActionResult Create(ShareIdea.Models.IdeaModel model)
+        {
+            try
+            {
+                if (model.Title != null)
+                {
+                    ShareIdeaDBDataContext cont = new ShareIdeaDBDataContext();
+                    Idea idea = new Idea()
+                    {
+                        title = model.Title,
+                        description = model.Description
+                    };
+                    cont.Ideas.InsertOnSubmit(idea);
+                    cont.SubmitChanges();
+                }
+                return RedirectToAction("Create");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Idea/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Idea/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Idea/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Idea/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShareIdea/Models/Idea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareIdea.Models
+{
+    public class IdeaModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Название")]
+        public string Title { get; set; }
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+        public int Choices { get; set; }
+    }
+}
